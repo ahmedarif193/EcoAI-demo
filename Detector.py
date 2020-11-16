323e3cd6591b456d0167fa3f9f5561913a44d969
Ahmed ARIF <arif193@gmail.com>
2020-11-16 21:55:42 +0000

    clean detector

--- Detector.py
+++ Detector.py
@@ -29,28 +29,7 @@ for detection in output[0, 0, :, :]:
     confidence = detection[2]
     if confidence > .3:
         class_id = detection[1]
-        class_name=id_class_name(class_id,classNames)
-#        print(str(str(class_id) + " " + str(detection[2])  + " " + class_name))
         if class_id == 1.0 :
             X = X + 1
-        if class_id == 17.0 :
-            Y = Y + 1
-        if class_id == 18.0 :
-            Z = Z + 1
-        box_x = detection[3] * image_width
-        box_y = detection[4] * image_height
-        box_width = detection[5] * image_width
-        box_height = detection[6] * image_height
-        cv2.rectangle(image, (int(box_x), int(box_y)), (int(box_width), int(box_height)), (23, 230, 210), thickness=1)
-        cv2.putText(image,class_name ,(int(box_x), int(box_y+.05*image_height)),cv2.FONT_HERSHEY_SIMPLEX,(2),(0, 0, 255))
-        #X == N
 
-
-
-# cv2.imwrite("image_box_text.jpg",image)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 print ("There are", X, "Cigarette butts")
-#print("There are", Y , "9touta")
-#print("There are", Z ,"klab")
